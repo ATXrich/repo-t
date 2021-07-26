d9fc00d3d72f0d87ef871d3dd9a81f2341e70310
rreed210 <richard_reed@comcast.com>
2021-07-26 14:11:46 -0500

    refactor dir structure

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
 
     #TODO: pull latest from master
 
-    # subprocess.run(["git","pull"])
-    process = subprocess.run(["ls","-l"], capture_output=True, text=True)
+    process = subprocess.run(["git","pull"], capture_output=True, text=True)
     if process.returncode != 0:
-        print('error: failed to pull latest from repo')
+        print(f'error: {process.stderr}')
         sys.exit(process.returncode)
     else:
-        process = subprocess.run(["git","log"], capture_output=True, text=True)
+        print(process.stdout)
+        # process = subprocess.run(["git","log"], capture_output=True, text=True)
         #TODO: git log last 24 hr commits
 
 
