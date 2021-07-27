4e5947edc5141cd950dc7871d447661e79b41b96
rreed210 <richard_reed@comcast.com>
2021-07-27 13:10:02 -0500

    XHFW-1234: testing this

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -1,7 +1,60 @@
 # # !/usr/bin/env python3
 
-import sys, subprocess
 
+# {
+#   "commit": "9caf3bc17f34ac3b93c8127dd776e34bad1d6ee0",
+#   "abbreviated_commit": "9caf3bc17",
+#   "tree": "bd69dee77adb7977a77d07dfcb02d39e3cab9066",
+#   "abbreviated_tree": "bd69dee77",
+#   "parent": "2798b17a396897b916a2f4a696b5726ff0e519dd",
+#   "abbreviated_parent": "2798b17a3",
+#   "refs": "HEAD -> master, origin/master, origin/HEAD",
+#   "encoding": "",
+#   "subject": "XHFW-2272: sync state request causes infinite loop of request",
+#   "sanitized_subject_line": "XHFW-2272-sync-state-request-causes-infinite-loop-of-request",
+#   "body": "Reason for change: If desired state is not set to null in xhf
+# thing full state report in response to sync request, it causes
+# server to send request again on reception of each response to
+# sync request and this loop never ends.
+
+# Test Procedure: Zith test has been fixed to detect it. It can be
+# tested on gateway using iot simulator and send sync request.
+
+# Risks: None
+
+# Change-Id: Iba3e4b684aa312ad90517e532356fcbf30ee3bd6
+# ",
+#   "commit_notes": "",
+#   "verification_flag": "N",
+#   "signer": "",
+#   "signer_key": "",
+#   "author": {
+#     "name": "Naeem Khan",
+#     "email": "naeem_khan@comcast.com",
+#     "date": "Thu, 17 Jun 2021 14:34:29 -0400"
+#   },
+#   "commiter": {
+#     "name": "Thomas Lea",
+#     "email": "Thomas_Lea@comcast.com",
+#     "date": "Wed, 30 Jun 2021 11:00:38 +0000"
+#   }
+# }
+
+
+
+import json
+import sys
+import subprocess
+import re
+
+body = 'Reason for change: Reason goes here. \
+        Test Procedure: Test Goes Here. \
+        Risks: None \
+        Change-Id: AODIE3r3fwwfd9'
+
+pretty_format = '{"commit": "%H", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE", "date": "%aD"}, "commiter": {"name": "%cN", "email": "%cE", "date": "%cD"}}'
+body_pattern = r''
+jira_ticket_pattern = r'XHFW-\d+'
 
 if __name__ == "__main__":
 
@@ -12,14 +65,19 @@ if __name__ == "__main__":
         print(f'error: {process.stderr}')
         sys.exit(process.returncode)
     else:
-        print(process.stdout)
-        # process = subprocess.run(["git","log"], capture_output=True, text=True)
         #TODO: git log last 24 hr commits
+        process = subprocess.run(["git","log","-n","1",f"--pretty=format:{pretty_format}"], capture_output=True, text=True)
+        print(process.stdout)
+        git_log_output = json.loads(process.stdout)
+        print(git_log_output["subject"])
 
+        # Dict = dict((x.strip(), y.strip())
+        #      for x, y in (element.split(':') 
+        #      for element in body.split(', ')))
 
+        # print(body)
+   
 
-    
-
-    #TODO: parse git comments
+    #TODO: parse git comments (subject and body)
 
     #TODO: upload to dynamo
