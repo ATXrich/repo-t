2f00f0ebe9dc899aa498c4ec5f200b04d592db0a
rreed210 <richard_reed@comcast.com>
2021-07-28 14:54:14 -0500

    XHFW-1234: adding extra file

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -1,83 +1,60 @@
-# # !/usr/bin/env python3
-
-
-# {
-#   "commit": "9caf3bc17f34ac3b93c8127dd776e34bad1d6ee0",
-#   "abbreviated_commit": "9caf3bc17",
-#   "tree": "bd69dee77adb7977a77d07dfcb02d39e3cab9066",
-#   "abbreviated_tree": "bd69dee77",
-#   "parent": "2798b17a396897b916a2f4a696b5726ff0e519dd",
-#   "abbreviated_parent": "2798b17a3",
-#   "refs": "HEAD -> master, origin/master, origin/HEAD",
-#   "encoding": "",
-#   "subject": "XHFW-2272: sync state request causes infinite loop of request",
-#   "sanitized_subject_line": "XHFW-2272-sync-state-request-causes-infinite-loop-of-request",
-#   "body": "Reason for change: If desired state is not set to null in xhf
-# thing full state report in response to sync request, it causes
-# server to send request again on reception of each response to
-# sync request and this loop never ends.
-
-# Test Procedure: Zith test has been fixed to detect it. It can be
-# tested on gateway using iot simulator and send sync request.
-
-# Risks: None
-
-# Change-Id: Iba3e4b684aa312ad90517e532356fcbf30ee3bd6
-# ",
-#   "commit_notes": "",
-#   "verification_flag": "N",
-#   "signer": "",
-#   "signer_key": "",
-#   "author": {
-#     "name": "Naeem Khan",
-#     "email": "naeem_khan@comcast.com",
-#     "date": "Thu, 17 Jun 2021 14:34:29 -0400"
-#   },
-#   "commiter": {
-#     "name": "Thomas Lea",
-#     "email": "Thomas_Lea@comcast.com",
-#     "date": "Wed, 30 Jun 2021 11:00:38 +0000"
-#   }
-# }
-
-
-
 import json
 import sys
 import subprocess
 import re
 
-body = 'Reason for change: Reason goes here. \
-        Test Procedure: Test Goes Here. \
-        Risks: None \
-        Change-Id: AODIE3r3fwwfd9'
 
-pretty_format = '{"commit": "%H", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE", "date": "%aD"}, "commiter": {"name": "%cN", "email": "%cE", "date": "%cD"}}'
-body_pattern = r''
-jira_ticket_pattern = r'XHFW-\d+'
+# build_number
+# branch_name
+# [developers]
+# git_logs - is just a JSON string, which we can leverage it into future use for reporting, if required.
 
-if __name__ == "__main__":
+# For Git log format, I prefer to consider -- "{ filename_1 : { jira_number, commiter, logs }, filename_2 : { jira_number, commiter, logs } }
 
-    #TODO: pull latest from master
 
-    process = subprocess.run(["git","pull"], capture_output=True, text=True)
-    if process.returncode != 0:
-        print(f'error: {process.stderr}')
-        sys.exit(process.returncode)
+
+pretty_format = '{"commit": "%H", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE", "date": "%at"}}'
+
+
+# sample_log = 
+
+def set_attribute(git_log, pattern, log_location):
+    attribute = re.search(pattern, git_log[log_location])
+    if attribute != None:
+        return attribute.group(1)
     else:
-        #TODO: git log last 24 hr commits
-        process = subprocess.run(["git","log","-n","1",f"--pretty=format:{pretty_format}"], capture_output=True, text=True)
-        print(process.stdout)
-        git_log_output = json.loads(process.stdout)
-        print(git_log_output["subject"])
+        return ''
 
-        # Dict = dict((x.strip(), y.strip())
-        #      for x, y in (element.split(':') 
-        #      for element in body.split(', ')))
 
-        # print(body)
-   
+def build_json_output(git_log):
+    git_log['jira_id'] = set_attribute(git_log, r'(XHFW-\d+)', 'subject')
+    git_log['reason_for_change'] = set_attribute(git_log, r'[c|C]hange:\s(.+)\sTest', 'body')
+    git_log['test_procedure'] = set_attribute(git_log, r'[p|P]rocedure:\s(.+)\s[r|R]isks', 'body')
+    git_log['risks'] = set_attribute(git_log,  r'[r|R]isks:\s(.+)\s[c|C]hange', 'body')
+    git_log['change_id'] = set_attribute(git_log, r'[c|C]hange-[i|I]d:\s(.+)', 'body')  
+        
+    print(git_log)
 
-    #TODO: parse git comments (subject and body)
+    # return json.dumps(git_log)
 
-    #TODO: upload to dynamo
+
+
+
+
+
+process = subprocess.run(['git','pull'], capture_output=True, text=True)
+if process.returncode != 0:
+    print(f'error: {process.stderr}')
+    sys.exit(process.returncode)
+else:
+    # fetch last commit
+    process = subprocess.run(['git', 'log', '-n', '1', '--raw'], capture_output=True, text=True)
+
+    # process = subprocess.run(['git', 'log', '-n', '1', '--raw', f'--pretty=format:{pretty_format}'], capture_output=True, text=True)
+
+    print(process.stdout)
+    #TODO: build json payload
+    # git_log = build_json_output(json.loads(process.stdout))
+
+
+#TODO: upload to dynamo
