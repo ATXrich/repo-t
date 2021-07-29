f337f9df47abb427a40a2d910052cbedf520e5b5
rreed210 <richard_reed@comcast.com>
2021-07-29 14:44:55 -0500

    xhfw-1234: updating script

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -8,12 +8,24 @@ import re
 # branch_name
 # [developers]
 # git_logs - is just a JSON string, which we can leverage it into future use for reporting, if required.
+# {
+#   commit:
+#   date:
+#   jira_id:
+#   committer: {
+#               name:
+#               email: 
+#   }
+#   filenames: {}
+#   subject:
+#   body:
+# }
 
 # For Git log format, I prefer to consider -- "{ filename_1 : { jira_number, commiter, logs }, filename_2 : { jira_number, commiter, logs } }
 
 
 
-pretty_format = '{"commit": "%H", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE", "date": "%at"}}'
+format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", "committer": {"name": "%cN", "email": "%cE"}}\''
 
 
 # sample_log = 
@@ -27,13 +39,27 @@ def set_attribute(git_log, pattern, log_location):
 
 
 def build_json_output(git_log):
+    # add Jira ID to log
     git_log['jira_id'] = set_attribute(git_log, r'(XHFW-\d+)', 'subject')
-    git_log['reason_for_change'] = set_attribute(git_log, r'[c|C]hange:\s(.+)\sTest', 'body')
-    git_log['test_procedure'] = set_attribute(git_log, r'[p|P]rocedure:\s(.+)\s[r|R]isks', 'body')
-    git_log['risks'] = set_attribute(git_log,  r'[r|R]isks:\s(.+)\s[c|C]hange', 'body')
-    git_log['change_id'] = set_attribute(git_log, r'[c|C]hange-[i|I]d:\s(.+)', 'body')  
-        
+
+    # get release version and add to log
+    process = subprocess.run([f'git status'], shell=True, capture_output=True, text=True)
+    build = re.search(r'release/(\d.+\d)'.group(1), process.stdout)
+    if build != None:
+        git_log['build'] = build
+    else:
+        git_log['build'] = ''
+
+    # capture changed files and add to log
+    changed_files = subprocess.run([f'git show --pretty="format:" --decorate --name-only {git_log["commit"]}'], shell=True, capture_output=True, text=True)
+    git_log['files'] = changed_files.stdout.splitlines()
+
+
+
     print(git_log)
+
+    
+    # print(files_in_git_log.stdout)
 
     # return json.dumps(git_log)
 
@@ -42,19 +68,20 @@ def build_json_output(git_log):
 
 
 
-process = subprocess.run(['git','pull'], capture_output=True, text=True)
+process = subprocess.run(['git pull'], shell=True, capture_output=True, text=True)
 if process.returncode != 0:
     print(f'error: {process.stderr}')
     sys.exit(process.returncode)
 else:
-    # fetch last commit
-    process = subprocess.run(['git', 'log', '-n', '1', '--raw'], capture_output=True, text=True)
+    # capture git logs in the past 24 hours
+    git_logs = subprocess.run(f'git log --since="24 hours ago" --format={format}', shell=True, capture_output=True, text=True).stdout.splitlines()
+    print(git_logs)
+    # if len(git_logs) > 0:
+    #     for log in git_logs:
+    #         #TODO: build json payload
+    #         payload = build_json_output(json.loads(log))
+        
 
-    # process = subprocess.run(['git', 'log', '-n', '1', '--raw', f'--pretty=format:{pretty_format}'], capture_output=True, text=True)
-
-    print(process.stdout)
-    #TODO: build json payload
-    # git_log = build_json_output(json.loads(process.stdout))
 
 
 #TODO: upload to dynamo
