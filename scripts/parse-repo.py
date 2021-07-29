6c32ba9f3f4d82c0d510c2806ba02d05289654d9
rreed210 <richard_reed@comcast.com>
2021-07-29 15:07:22 -0500

    XHaven-5184: clean up script

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -30,28 +30,24 @@ format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", "comm
 
 # sample_log = 
 
-def set_attribute(git_log, pattern, log_location):
-    attribute = re.search(pattern, git_log[log_location])
-    if attribute != None:
-        return attribute.group(1)
-    else:
-        return ''
-
-
 def build_json_output(git_log):
     # add Jira ID to log
-    git_log['jira_id'] = set_attribute(git_log, r'(XHFW-\d+)', 'subject')
+    jira_id = re.search(r'([a-zA-Z]+-\d+)', git_log['subject']).group(1)
+    if jira_id != None:
+        git_log['jira_id'] = jira_id
+    else:
+        git_log['jira_id'] = ''
 
     # get release version and add to log
     process = subprocess.run([f'git status'], shell=True, capture_output=True, text=True)
-    build = re.search(r'release/(\d.+\d)'.group(1), process.stdout)
+    build = re.search(r'release/(\d.+\d)', process.stdout).group(1)
     if build != None:
         git_log['build'] = build
     else:
         git_log['build'] = ''
 
     # capture changed files and add to log
-    changed_files = subprocess.run([f'git show --pretty="format:" --decorate --name-only {git_log["commit"]}'], shell=True, capture_output=True, text=True)
+    changed_files = subprocess.run([f'git show --pretty="format:" --name-only {git_log["commit"]}'], shell=True, capture_output=True, text=True)
     git_log['files'] = changed_files.stdout.splitlines()
 
 
@@ -75,11 +71,11 @@ if process.returncode != 0:
 else:
     # capture git logs in the past 24 hours
     git_logs = subprocess.run(f'git log --since="24 hours ago" --format={format}', shell=True, capture_output=True, text=True).stdout.splitlines()
-    print(git_logs)
-    # if len(git_logs) > 0:
-    #     for log in git_logs:
-    #         #TODO: build json payload
-    #         payload = build_json_output(json.loads(log))
+    # print(git_logs)
+    if len(git_logs) > 0:
+        for log in git_logs:
+            #TODO: build json payload
+            payload = build_json_output(json.loads(log))
         
 
 
