a64964240420b5c019c493fa1442cf132d1eb710
rreed210 <richard_reed@comcast.com>
2021-07-30 09:26:11 -0500

    XHAVEN-5184: removing old file and clean up script

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -1,80 +1,56 @@
 import json
-import sys
 import subprocess
 import re
 
 
-# build_number
-# branch_name
-# [developers]
-# git_logs - is just a JSON string, which we can leverage it into future use for reporting, if required.
-# {
-#   commit:
-#   date:
-#   jira_id:
-#   committer: {
-#               name:
-#               email: 
-#   }
-#   filenames: {}
-#   subject:
-#   body:
-# }
+format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE"}, "committer": {"name": "%cN", "email": "%cE"}}\''
 
-# For Git log format, I prefer to consider -- "{ filename_1 : { jira_number, commiter, logs }, filename_2 : { jira_number, commiter, logs } }
+
+def search_git_output(regex, output):
+    result = re.search(regex, output).group(1)
+    if result != None:
+        return result
+    else:
+        return ''
 
 
 
-format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", "committer": {"name": "%cN", "email": "%cE"}}\''
+def build_json_payload(git_log):
+    payload = {}
 
-
-# sample_log = 
-
-def build_json_output(git_log):
     # add Jira ID to log
-    jira_id = re.search(r'([a-zA-Z]+-\d+)', git_log['subject']).group(1)
-    if jira_id != None:
-        git_log['jira_id'] = jira_id
-    else:
-        git_log['jira_id'] = ''
-
-    # get release version and add to log
+    git_log['jira_id'] = search_git_output(r'([a-zA-Z]+-\d+)', git_log['subject'])
+    
+    # add build and branch to log
     process = subprocess.run([f'git status'], shell=True, capture_output=True, text=True)
-    build = re.search(r'release/(\d.+\d)', process.stdout).group(1)
-    if build != None:
-        git_log['build'] = build
-    else:
-        git_log['build'] = ''
-
+    payload['build'] = search_git_output(r'release/(\d.+\d)', process.stdout)
+    payload['branch'] = search_git_output(r'branch\s(.+)', process.stdout)
+    
     # capture changed files and add to log
     changed_files = subprocess.run([f'git show --pretty="format:" --name-only {git_log["commit"]}'], shell=True, capture_output=True, text=True)
-    git_log['files'] = changed_files.stdout.splitlines()
+    git_log['filenames'] = changed_files.stdout.splitlines()
 
-    return json.dumps(git_log)
+    payload['developer'] = git_log['author']['name']
+    payload['git_log'] = git_log
+
+    return payload
 
 
 
 
 
-
-# process = subprocess.run(['git pull'], shell=True, capture_output=True, text=True)
-# if process.returncode != 0:
-#     print(f'error: {process.stderr}')
-#     sys.exit(process.returncode)
-# else:
 # capture git logs in the past 24 hours
-git_logs = subprocess.run(f'git log --since="1 hour ago" --format={format}', shell=True, capture_output=True, text=True).stdout.splitlines()
-# print(git_logs)
+git_logs = subprocess.run(f'git log --since="24 hours ago" --format={format}', shell=True, capture_output=True, text=True).stdout.splitlines()
 if len(git_logs) > 0:
     for log in git_logs:
         #TODO: build json payload
-        payload = build_json_output(json.loads(log))
+        payload = build_json_payload(json.loads(log))
 
-        print(f'To be sent to DynamoDB: {payload} ({type(payload)})')
+        #TODO: upload to dynamo
+        print(f'To be sent to DynamoDB: {payload}')
 else:
     print("No new commits in last 24 hours")
         
 
 
 
-#TODO: upload to dynamo
