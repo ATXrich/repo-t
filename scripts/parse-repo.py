7f56b3905da65dd2f3ed51d24b6528d196c8c3ff
rreed210 <richard_reed@comcast.com>
2021-07-30 10:41:14 -0500

    XHAVEN-5184: pep-8 updates

--- scripts/parse-repo.py
+++ scripts/parse-repo.py
@@ -3,16 +3,16 @@ import subprocess
 import re
 
 
-format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", "author": {"name": "%aN", "email": "%aE"}, "committer": {"name": "%cN", "email": "%cE"}}\''
+format = '\'{"commit": "%h", "date": "%ct", "subject": "%s", "body": "%b", \
+        "author": {"name": "%aN", "email": "%aE"}, "committer": {"name": "%cN", "email": "%cE"}}\''
 
 
 def search_git_output(regex, output):
     result = re.search(regex, output).group(1)
-    if result != None:
+    if result is not None:
         return result
     else:
         return ''
-
 
 
 def build_json_payload(git_log):
@@ -27,7 +27,9 @@ def build_json_payload(git_log):
     payload['branch'] = search_git_output(r'branch\s(.+)', process.stdout)
     
     # capture changed files and add to log
-    changed_files = subprocess.run([f'git show --pretty="format:" --name-only {git_log["commit"]}'], shell=True, capture_output=True, text=True)
+    changed_files = subprocess.run(
+        [f'git show --pretty="format:" --name-only {git_log["commit"]}'], 
+        shell=True, capture_output=True, text=True)
     git_log['filenames'] = changed_files.stdout.splitlines()
 
     payload['developer'] = git_log['author']['name']
@@ -36,21 +38,15 @@ def build_json_payload(git_log):
     return payload
 
 
-
-
-
 # capture git logs in the past 24 hours
-git_logs = subprocess.run(f'git log --since="24 hours ago" --format={format}', shell=True, capture_output=True, text=True).stdout.splitlines()
+git_logs = subprocess.run(
+    f'git log --since="24 hours ago" --format={format}', 
+    shell=True, capture_output=True, text=True).stdout.splitlines()
 if len(git_logs) > 0:
     for log in git_logs:
-        #TODO: build json payload
         payload = build_json_payload(json.loads(log))
 
-        #TODO: upload to dynamo
+        # TODO: upload to dynamo
         print(f'To be sent to DynamoDB: {payload}')
 else:
     print("No new commits in last 24 hours")
-        
-
-
-
