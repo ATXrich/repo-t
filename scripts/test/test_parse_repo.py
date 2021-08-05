027d41f17ac89b4bd867108a0f2bcef6e653e532
rreed210 <richard_reed@comcast.com>
2021-08-05 13:44:51 -0500

    XHAVEN-5184: complete search git log unit tests

--- scripts/test/test_parse_repo.py
+++ scripts/test/test_parse_repo.py
@@ -1,8 +1,8 @@
-import re
 import unittest
 from dataclasses import dataclass
 from typing import List
 import parse_repo
+
 
 class TestParseRepo(unittest.TestCase):
     """Tests for 'parse_repo.py'."""
@@ -10,20 +10,22 @@ class TestParseRepo(unittest.TestCase):
         @dataclass
         class TestCase:
             name: str
-            input: List[any]
+            input: List[str]
             expected: str
 
         regex = r'([a-zA-Z]+-\d+)'
         testcases = [
-            TestCase(name="Empty subject", input=[regex, {"commit": "a1254", "subject": "", "body": ""}], expected=""),
-            # TestCase(name="Title Jira ID", input=[regex, {"subject": "Xhfw-1234: This is a test"}], expected="XHFW-1234"),
-            # TestCase(name="lower Jira ID", input=[regex, {"subject": "xhfw-1234: This is a test"}], expected="XHFW-1234"),
-            # TestCase(name="UPPER Jira ID", input=[regex, {"subject": "XHFW-1234: This is a test"}], expected="XHFW-1234"),
-            # TestCase(name="no Jira ID", input=[regex, {"subject": "This is a test"}], expected=""),
+            TestCase(name='Empty subject', input=[regex, ''], expected=''),
+            TestCase(name='Title Jira ID', input=[regex, 'Xhfw-1234: This is a test'], expected='XHFW-1234'),
+            TestCase(name='lower Jira ID', input=[regex, 'xhfw-1234: This is a test'], expected='XHFW-1234'),
+            TestCase(name='UPPER Jira ID', input=[regex, 'XHFW-1234: This is a test'], expected='XHFW-1234'),
+            TestCase(name='no Jira ID', input=[regex, 'unit test file'], expected=''),
+            TestCase(name='Partial Jira ID', input=[regex, 'XH1245 - unit test file'], expected=''),
+            TestCase(name='No colon', input=[regex, 'XHFW-1234 unit test file'], expected='XHFW-1234'),
         ]
 
         for case in testcases:
-            actual = parse_repo.search_git_log(case.input[0], case.input[1])
+            actual = parse_repo.search_git_log(case.input[0], case.input[1]).upper()
             self.assertEqual(
                 case.expected,
                 actual,
