38974bad69ac13e514fc4fd84c0a43c83baee161
rreed210 <richard_reed@comcast.com>
2021-08-08 15:46:01 -0500

    xhaven-5184: dynamodb tests

--- scripts/test/test_parse_repo.py
+++ scripts/test/test_parse_repo.py
@@ -1,10 +1,49 @@
 import unittest
 from dataclasses import dataclass
 from typing import List
+import json
+import boto3
+from moto import mock_dynamodb2
+from botocore.exceptions import ClientError
 import parse_repo
 
-
+@mock_dynamodb2
 class TestParseRepo(unittest.TestCase):
+    table_item = {}
+    build_number = ""
+
+
+    def setUp(self, dynamodb=None):
+        if not dynamodb:
+            dynamo_db = boto3.resource('dynamodb', region_name='us-east-2')
+        table_name = 'Repo_T_Execution_History'
+        self.table = dynamo_db.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'build_number',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'build_number',
+                    'AttributeType': 'S'
+                }
+            ])
+
+        with open('test/data/data.json') as json_file:
+            data = json.load(json_file)
+            self.table_item = data
+            self.build_number = data["build_number"]
+        self.table.put_item(Item=data)
+
+
+    def tearDown(self):
+        self.table.delete()
+        self.dynamodb=None
+
+
     def test_search_git_log(self):
         @dataclass
         class TestCase:
@@ -58,3 +97,16 @@ class TestParseRepo(unittest.TestCase):
                 actual,
                 f'failed test {case.name} expected {case.expected}, actual {actual}'
             )
+
+
+    def test_get_developers_from_dynamodb(self):
+        actual_output = parse_repo.get_developers_from_dynamodb(self.build_number)
+        self.assertEqual(actual_output, self.table_item['developers'])
+    
+    def test_update_dynamodb(self):
+        payload = [{"git_logs": {"commit": "be84c43", "date": "Sat Aug 7 16:24:40 2021 -0500", "subject": "xhaven-5184: clean up test table", "body": "", "author": {"name": "rreed210", "email": "richard_reed@comcast.com"}, "jira_id": "XHAVEN-5184", "filenames": ["scripts/test/test_parse_repo.py"]}}, {"git_logs": {"commit": "4065637", "date": "Sat Aug 7 16:22:33 2021 -0500", "subject": "xhaven-5184 : removed helper method", "body": "", "author": {"name": "rreed210", "email": "richard_reed@comcast.com"}, "jira_id": "XHAVEN-5184", "filenames": ["scripts/parse_repo.py"]}}, {"git_logs": {"commit": "71b91fa", "date": "Sat Aug 7 15:46:12 2021 -0500", "subject": "xhaven-5184: adding helper func to show db update", "body": "", "author": {"name": "rreed210", "email": "richard_reed@comcast.com"}, "jira_id": "XHAVEN-5184", "filenames": ["scripts/parse_repo.py"]}}, {"git_logs": {"commit": "7f6887b", "date": "Sat Aug 7 15:34:50 2021 -0500", "subject": "XHAVEN-5184: adding git log by dev", "body": "", "author": {"name": "rreed210", "email": "richard_reed@comcast.com"}, "jira_id": "XHAVEN-5184", "filenames": ["scripts/parse_repo.py"]}}]
+        actual_output = parse_repo.update_dynamodb(json.dumps(payload), self.build_number)
+        self.assertEqual(f'Updated table with most recent git logs.', actual_output)
+    
+if __name__ == '__main__':
+    unittest.main()
