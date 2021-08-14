e0c816ea356f958c1e7d56f93c3bc99225cba1bc
rreed210 <richard_reed@comcast.com>
2021-08-14 09:29:07 -0500

    xhaven-5184: refactoring

--- scripts/parse_repo.py
+++ scripts/parse_repo.py
@@ -27,25 +27,41 @@ def parse_git_logs(branch_name: str) -> str:
     """Checks for new git commits in last 24 hours for given branch and developer(s). 
        Formats output and uploads to dynamodb table."""
 
-    payload = []
+    # payload = []
 
-    # branch_name = get_value_from_dynamodb(repo_t_tables[0]['table_name'], repo_t_tables[0]['p_key'], 'gerrit_branch_name')
-    developers = get_value_from_dynamodb(repo_t_tables[0]['table_name'], repo_t_tables[0]['p_key'], branch_name, 'developers')
-    for developer in developers:
-        # capture git commits 24 hours ago for given developer and branch
-        process = subprocess.run(
-            f'git log --author={developer} --since="24 hours ago" --format={format} {branch_name}', 
-            shell=True, capture_output=True, text=True)
+    cpe_branch_details = get_item_from_dynamodb(repo_t_tables[0]['table_name'], 
+                                                repo_t_tables[0]['p_key'], 
+                                                branch_name)
 
-        git_logs = process.stdout.splitlines()
+    payload = build_dynamodb_payload(json.dumps(cpe_branch_details))
+
+    print('Payload to be uploaded to db table: ', payload, type(payload))
+
+    # upload history to dynamodb
+    # response = update_dynamodb(repo_t_tables[1]['table_name'], repo_t_tables[1]['p_key'], payload['build_number'], payload)
+    
+    return response
+
+
+    # developers = get_value_from_dynamodb(repo_t_tables[0]['table_name'], 
+    #                                      repo_t_tables[0]['p_key'], 
+    #                                      branch_name, 'developers')
+    # for developer in developers:
+    #     # capture git commits 24 hours ago for given developer and branch
+    #     process = subprocess.run(
+    #         f'git log --author={developer} --since="24 hours ago" --format={format} {branch_name}', 
+    #         shell=True, capture_output=True, text=True)
+
+    #     git_logs = process.stdout.splitlines()
         
-        print(f'Retrieved {len(git_logs)} commits in last 24 hours for {developer} on branch {branch_name}.')
+    #     print(f'Retrieved {len(git_logs)} commits in last 24 hours for {developer} on branch {branch_name}.')
 
     #     # build payload for dynamodb
     #     if len(git_logs) > 0:
     #         for git_log in git_logs:
-    #             dynamodb_item = build_dynamodb_item(json.loads(git_log))
-    #             payload.append(dynamodb_item)
+    #             print(git_log)
+                # dynamodb_item = build_dynamodb_item(json.loads(git_log))
+                # payload.append(dynamodb_item)
 
     # # upload git logs to dynamodb
     # if len(payload) > 0:
@@ -55,7 +71,43 @@ def parse_git_logs(branch_name: str) -> str:
     # return response
 
 
-def get_value_from_dynamodb(table_name: str, primary_key: str, pkey_value: str, attribute: str):
+def build_dynamodb_payload(cpe_branch_details: dict) -> dict:
+    payload = {}
+    git_log_list = []
+
+    payload['branch_name'] = cpe_branch_details['gerrit_branch_name']
+    payload['build_number'] = cpe_branch_details['build_version']
+    payload['current_page'] = 1
+    payload['date_time'] =] = '2021-08-14'
+    payload['developers'] = cpe_branch_details['developers']
+    payload['fail_test_cases'] = 0 
+    payload['git_logs'] = git_log_list
+    payload['inventory_board'] = cpe_branch_details['inventory_board']
+    payload['nexus_url'] = cpe_branch_details['nexus_url']
+    payload['page_size'] = 50
+    payload['pass_test_cases'] = '50'
+    payload['test_cycle'] = 'sample_test_cycle'
+    payload['total_pages'] = 3
+    payload['total_test_cases'] = '150' 
+
+    for developer in payload['developers']:
+        # capture git commits 24 hours ago for given developer and branch
+        process = subprocess.run(
+            f'git log --author={developer} --since="24 hours ago" --format={format} {payload["branch_name"]}', 
+            shell=True, capture_output=True, text=True)
+
+        git_logs = process.stdout.splitlines()
+        
+        print(f'Retrieved {len(git_logs)} commits in last 24 hours for {developer} on branch {payload["branch_name"]}.')
+
+        # build git log list for payload['git_logs']
+        if len(git_logs) > 0:
+            for git_log in git_logs:
+                # print(git_log)
+                formatted_git_logs = format_git_logs(json.loads(git_log))
+                git_log_list.append(formatted_git_logs)
+
+def get_item_from_dynamodb(table_name: str, primary_key: str, pkey_value: str):
     """Returns value for a given dynamodb item attribute"""
 
     try:
@@ -67,18 +119,13 @@ def get_value_from_dynamodb(table_name: str, primary_key: str, pkey_value: str, 
                 primary_key: pkey_value
             }
         )
-        attribute = response['Item'][attribute]
-        if len(attribute) > 0:
-            return attribute
-        else:
-            print(f'{attribute} not found for key: {primary_key}')
-            exit(1)
+        return response['Item']
     except Exception as e:
         print(f'error fetching data from dynamodb: {e}')
         exit(1)
 
 
-def build_dynamodb_item(git_log: dict) -> dict:
+def format_git_logs(git_log: dict) -> dict:
     """Adds additional attributes to git log before updating dynamodb table item"""
 
     # add Jira ID to log
@@ -110,7 +157,7 @@ def search_git_log(regex: str, output: str) -> str:
         return ''
 
 
-def update_dynamodb(table_name: str, build_number: str, payload: list) -> str:
+def update_dynamodb(table_name: str, primary_key: str, pkey_value: str, payload: dict) -> str:
     """Writes payload to dynamodb table."""
 
     dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
@@ -118,7 +165,7 @@ def update_dynamodb(table_name: str, build_number: str, payload: list) -> str:
         table = dynamodb.Table(table_name)
         response = table.update_item(
             Key={
-                'build_number': build_number
+                primary_key: pkey_value
             },
             UpdateExpression="SET git_logs=:g",
             ExpressionAttributeValues={
